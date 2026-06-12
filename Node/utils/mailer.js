580272ff3b0aec22840e5e72b4ea4b1968156287
sufianeh7 <sufianeh8@gmail.com>
2026-06-12 09:30:29 +0200

    configuración de nodemailer para render

--- Node/utils/mailer.js
+++ Node/utils/mailer.js
@@ -2,10 +2,16 @@ const nodemailer = require('nodemailer');
 
 // 1. Configurar el "Transportador" con los datos de tu Gmail
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: 'smtp.gmail.com',
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
+  },
+  connectionTimeout: 10000,
+  tls: {
+    rejectUnauthorized: false
   }
 });
 
