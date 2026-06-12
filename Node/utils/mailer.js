3302e7ba54da6e722cff737626a90b7c460579a8
sufianeh7 <sufianeh8@gmail.com>
2026-06-12 10:15:56 +0200

    forzar interfaz de red local a ipv4 con 0.0.0.0

--- Node/utils/mailer.js
+++ Node/utils/mailer.js
@@ -1,18 +1,15 @@
-const dns = require('dns');
-dns.setDefaultResultOrder('ipv4first');
-
 const nodemailer = require('nodemailer');
 
 // 1. Configurar el "Transportador" con los datos de tu Gmail
 const transporter = nodemailer.createTransport({
   host: 'smtp.gmail.com',
-  port: 587,
-  secure: false,
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
   },
-  family: 4,
+  localAddress: '0.0.0.0',
   tls: {
     rejectUnauthorized: false
   }
